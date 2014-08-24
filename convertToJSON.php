e4b78c376a286640925882bb970af4ac5c7bada2
Zhang Yichuan <zyc452478749@yahoo.com.sg>
2014-08-24 16:42:40 +0800

    Can show pie graph

---
+++ convertToJSON.php
@@ -0,0 +1,8 @@
+<?php
+
+$data = array('fields' => array('Photo', 'Video', 'SWF', 'Link', 'Status'),
+				'data' => array(0.5, 0.1, 0.25, 0.36, 0.24));
+
+	print json_encode($data);
+
+?>
---
+++ pieGraph.js
@@ -0,0 +1,51 @@
+
+$(function () {
+    var option = {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: 1,//null,
+            plotShadow: false
+        },
+        title: {
+            text: 'Type of your posts'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                }
+            }
+        },
+        series: [{
+            
+        }]
+    }
+
+    $.getJSON( "convertToJSON.php", function( data ) {
+        var elements = new Array();
+
+        for (var i = data.fields.length - 1; i >= 0; i--) {
+            var field = data.fields[i];
+            var ratio = data.data[i];
+            elements.push([field, ratio]);
+        };
+
+        option.series.push({
+            type: 'pie',
+            name: 'Posts type',
+            data: elements
+        })
+
+        $('#container').highcharts(option);
+    });
+    
+});
