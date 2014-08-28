4ff0434ec80d013f813cda4cad57d974ab38481c
tang <tangning111@gmail.com>
2014-08-29 00:43:54 +0800

    responsive design

--- js/core.js
+++ js/core.js
@@ -11,4 +11,35 @@ $(function(){
 
 	FB.Event.subscribe('auth.authResponseChange', onAuthResponseChange);
 	FB.Event.subscribe('auth.statusChange', onStatusChange);
+
+  //FB.Canvas.setSize();
+
+	$('.ignore-default').click(function(e){
+		e.preventDefault();
+	});
+
+	switchView('#front-page');
 });
+
+function switchView(view) {
+  $('.view').hide();
+  $(view).show();
+}
+
+function renderMe() {
+  var user = $('#user');
+  user.find('#photo').attr('src',friendCache.me.picture.data.url);
+}
+
+function renderFriends() {
+  var list = $('.scrollable_list');
+  list.children().remove('.item');
+  var template = list.find('.template'); 
+  for( var i = 0; i < friendCache.friends.length; i++ ) {
+    var item = template.clone().removeClass('template').addClass('item');
+    item.attr('data-id',friendCache.friends[i].id);
+    item.find('.name').html(friendCache.friends[i].name);
+    item.find('.profile').attr('src',friendCache.friends[i].picture.data.url);
+    list.append(item);
+  }
+}
--- js/ui.js
+++ js/ui.js
@@ -1,14 +1,12 @@
 function renderMe() {
   var user = $('#user');
-  // user.find('.name').html(friendCache.me.name);
   user.find('#photo').attr('src',friendCache.me.picture.data.url);
 }
 
 function renderFriends() {
   var list = $('.scrollable_list');
   list.children().remove('.item');
-  var template = list.find('.template');
-  // console.log("TEST " + friendCache.friends.length);
+  var template = list.find('.template'); 
   for( var i = 0; i < friendCache.friends.length; i++ ) {
     var item = template.clone().removeClass('template').addClass('item');
     item.attr('data-id',friendCache.friends[i].id);
@@ -16,4 +14,9 @@ function renderFriends() {
     item.find('.profile').attr('src',friendCache.friends[i].picture.data.url);
     list.append(item);
   }
+}
+
+function switchView(view) {
+  $('.view').hide();
+  $(view).show();
 }
