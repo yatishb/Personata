b2725bf7594669b8e1bbcce6a27688f6693d5b7b
tang <tangning111@gmail.com>
2014-08-30 14:43:18 +0800

    add pie graph

--- js/graph.js
+++ js/graph.js
@@ -182,3 +182,52 @@ function renderMonthDataGraph(){
         $('#monthly-container').highcharts(option);
     });
 }
+
+function renderDailyDataGraph(){
+    $.getJSON( "../pieGraphJSON.php", function( data ) {
+        var elements = new Array();
+
+        for (var i = data.fields.length - 1; i >= 0; i--) {
+            var field = data.fields[i];
+            var ratio = data.data[i];
+            elements.push([field, ratio]);
+        };
+
+        var option = {
+            chart: {
+                backgroundColor:'rgba(255, 255, 255, 0.2)',
+            },
+            credits: {
+                //enabled: false
+                text: 'Personata',
+                href: 'https://apps.facebook.com/personata-app/'
+            },
+            title: {
+                text: ''
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                        style: {
+                            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                        }
+                    }
+                }
+            },
+            series: [{
+                type: 'pie',
+                name: 'Posts type',
+                data: elements
+            }]
+        }
+ 
+        $('#daily-container').highcharts(option);
+    });
+}
