58d4131727666567a1241fe809a573a9bd4d86c5
tang <tangning111@gmail.com>
2014-08-30 15:41:10 +0800

    remove center div bg

--- js/graph.js
+++ js/graph.js
@@ -116,6 +116,70 @@ function renderEventsGraph() {
         });
 }
 
+function renderEventsGraph1() {
+    $('#events-container').highcharts({
+
+        chart: {
+            type: 'heatmap',
+            backgroundColor:'rgba(255, 255, 255, 0.2)',
+        },
+
+        title: {
+            text: ''
+        },
+
+        xAxis: {
+            categories: ['', '', '', '', '', '']
+        },
+
+        yAxis: {
+            categories: ['Period 1', 'Period 2', 'Period 3', 'Period 4', 'Period 5'],
+            title: null
+        },
+
+        colorAxis: {
+            min: 0,
+            minColor: '#FFFFFF',
+            maxColor: Highcharts.getOptions().colors[0]
+        },
+
+        legend: {
+            align: 'right',
+            layout: 'vertical',
+            margin: 0,
+            verticalAlign: 'top',
+            y: 25,
+            symbolHeight: 320
+        },
+
+        tooltip: {
+            formatter: function () {
+                return '<b>' + this.series.xAxis.categories[this.point.x] + '</b> sold <br><b>' +
+                    this.point.value + '</b> items on <br><b>' + this.series.yAxis.categories[this.point.y] + '</b>';
+            }
+        },
+
+        series: [{
+            name: 'Events Per Day',
+            borderWidth: 1,
+            data: [[0, 0, 10], [0, 1, 19], [0, 2, 8], [0, 3, 24], [0, 4, 67], [1, 0, 92],
+                    [1, 1, 58], [1, 2, 78], [1, 3, 117], [1, 4, 48], [2, 0, 35], [2, 1, 15], 
+                    [2, 2, 123], [2, 3, 64], [2, 4, 52], [3, 0, 72], [3, 1, 132], [3, 2, 114], 
+                    [3, 3, 19], [3, 4, 16], [4, 0, 38], [4, 1, 5], [4, 2, 8], [4, 3, 117], 
+                    [4, 4, 115], [5, 0, 88], [5, 1, 32], [5, 2, 12], [5, 3, 6], [5, 4, 120]],
+            dataLabels: {
+                enabled: true,
+                color: 'black',
+                style: {
+                    textShadow: 'none',
+                    HcTextStroke: null
+                }
+            }
+        }]
+
+    });
+}
+
 function renderMonthDataGraph(){
     $.getJSON( "../lineGraphJSON.php", function( data ) {
         var option = {
