fda3dc8e0ea4f02eede2fc3df5388135ddb73e02
tang <tangning111@gmail.com>
2014-08-31 18:21:18 +0800

    add share function

--- js/core.js
+++ js/core.js
@@ -21,45 +21,63 @@ $(function(){
 	switchView('#front-page');
 
   $('#share').click(function () {
-    var chart = $('#monthly-container').highcharts(),
-    svg = chart.getSVG();
-
-    var canvas = document.createElement( "canvas" );
-    var ctx = canvas.getContext( "2d" );
-    var img = document.createElement( "img" );
-    img.setAttribute( "src", "data:image/svg+xml;base64," + btoa(svg));
-    ctx.canvas.width  = img.width;
-    ctx.canvas.height = img.height;
-    img.onload = function() {
-      ctx.drawImage(img, 0, 0);
-      var photo = canvas.toDataURL("img/png");
-
+      var obj = {}, chart;
+          
+      chart = $('#monthly-container').highcharts();
+      obj.svg = chart.getSVG();
+      obj.type = 'image/png';
+      obj.width = 450; 
+      obj.async = true;
+      
       $.ajax({
-        type: "POST",
-        url: "saveImage.php",
-        data: {
-          uid: friendCache.me.id,
-          imgBase64: photo
-        }
-      }).done(function(o) {
-        console.log(o);
-        FBSharePhoto();
-        // If you want the file to be visible in the browser 
-        // - please modify the callback in javascript. All you
-        // need is to return the url to the file, you just saved 
-        // and than put the image in your browser.
+          type: 'post',
+          url: chart.options.exporting.url,        
+          data: obj, 
+          success: function (data) {            
+              var exportUrl = this.url;
+              FBSharePhoto(exportUrl+data);
+          }        
       });
-    };
+
+
+    // var svg = chart.getSVG();
+
+    // var canvas = document.createElement( "canvas" );
+    // var ctx = canvas.getContext( "2d" );
+    // var img = document.createElement( "img" );
+    // img.setAttribute( "src", "data:image/svg+xml;base64," + btoa(svg));
+    // ctx.canvas.width  = img.width;
+    // ctx.canvas.height = img.height;
+    // img.onload = function() {
+    //   ctx.drawImage(img, 0, 0);
+    //   var photo = canvas.toDataURL("image/png");
+    //   console.log(photo);
+    //   $.ajax({
+    //     type: "POST",
+    //     url: "saveImage.php",
+    //     data: {
+    //       uid: friendCache.me.id,
+    //       imgBase64: photo
+    //     }
+    //   }).done(function(o) {
+    //     console.log(o);
+    //     FBSharePhoto();
+    //     // If you want the file to be visible in the browser 
+    //     // - please modify the callback in javascript. All you
+    //     // need is to return the url to the file, you just saved 
+    //     // and than put the image in your browser.
+    //   });
+    // };
   });
 });
 
-function FBSharePhoto(){
+function FBSharePhoto(url){
   FB.ui(
     {
       method: 'feed',
       name: 'Facebook Dialogs',
       link: 'http://54.254.165.1/dev/',
-      picture: 'http://54.254.165.1/dev/images/' + friendCache.me.id + '.png',
+      picture: url,//'http://54.254.165.1/dev/images/' + friendCache.me.id + '.png',
       caption: 'Reference Documentation',
       description: 'Dialogs provide a simple, consistent interface for applications to interface with users.'
     },
