bc076ec985cbd5cbc6099bcfdbeb2b91cbed1a88
Zhang Yichuan <zyc452478749@yahoo.com.sg>
2014-08-31 15:06:40 +0800

    Can share image to Facebook now

--- js/core.js
+++ js/core.js
@@ -19,7 +19,61 @@ $(function(){
 	});
 
 	switchView('#front-page');
+
+  $('#share').click(function () {
+    console.log("come in to share");
+    var chart = $('#monthly-container').highcharts(),
+    svg = chart.getSVG();
+
+    var canvas = document.createElement( "canvas" );
+    var ctx = canvas.getContext( "2d" );
+    var img = document.createElement( "img" );
+    img.setAttribute( "src", "data:image/svg+xml;base64," + btoa(svg));
+    ctx.canvas.width  = img.width;
+    ctx.canvas.height = img.height;
+    img.onload = function() {
+      ctx.drawImage(img, 0, 0);
+      var photo = canvas.toDataURL();
+      // window.open( canvas.toDataURL( "image/png" ) );
+      console.log("come to call share to facebook function");
+      $.ajax({
+        type: "POST",
+        url: "saveImage.php",
+        data: {
+          uid: friendCache.me.id,
+          imgBase64: photo
+        }
+      }).done(function(o) {
+        console.log(o);
+        FBSharePhoto();
+        // If you want the file to be visible in the browser 
+        // - please modify the callback in javascript. All you
+        // need is to return the url to the file, you just saved 
+        // and than put the image in your browser.
+      });
+    };
+  });
 });
+
+function FBSharePhoto(){
+  FB.ui(
+    {
+      method: 'feed',
+      name: 'Facebook Dialogs',
+      link: 'http://54.254.165.1/dev/',
+      picture: 'http://54.254.165.1/dev/img/' + friendCache.me.id + '.png',
+      caption: 'Reference Documentation',
+      description: 'Dialogs provide a simple, consistent interface for applications to interface with users.'
+    },
+    function(response) {
+      if (response && response.post_id) {
+        alert('Post was published.');
+      } else {
+        alert('Post was not published.');
+      }
+    }
+  );
+}
 
 function FBInvite(){
     FB.ui({method:'apprequests',
