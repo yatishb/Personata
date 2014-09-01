11ae399bcf7407582e1103c72f560a0e27a5d877
tang <tangning111@gmail.com>
2014-09-01 17:55:25 +0800

    add log in page

--- js/core.js
+++ js/core.js
@@ -12,13 +12,21 @@ $(function(){
 	FB.Event.subscribe('auth.authResponseChange', onAuthResponseChange);
 	FB.Event.subscribe('auth.statusChange', onStatusChange);
 
-  FB.Canvas.setSize();
-
 	$('.ignore-default').click(function(e){
 		e.preventDefault();
 	});
 
-	switchView('#front-page');
+  if (!isLoggedIn) {
+    console.log(isLoggedIn);
+    $('#main').hide();
+    $('#login-page').show();
+  } else {
+    switchView('#front-page');
+  }
+	
+  $('#fb-login').click(function(){
+    login(loginCallback);
+  });
 
   $('#share').click(function () {
       var obj = {}, chart;
@@ -38,73 +46,12 @@ $(function(){
               FBSharePhoto(exportUrl+data);
           }        
       });
-
-
-    // var svg = chart.getSVG();
-
-    // var canvas = document.createElement( "canvas" );
-    // var ctx = canvas.getContext( "2d" );
-    // var img = document.createElement( "img" );
-    // img.setAttribute( "src", "data:image/svg+xml;base64," + btoa(svg));
-    // ctx.canvas.width  = img.width;
-    // ctx.canvas.height = img.height;
-    // img.onload = function() {
-    //   ctx.drawImage(img, 0, 0);
-    //   var photo = canvas.toDataURL("image/png");
-    //   console.log(photo);
-    //   $.ajax({
-    //     type: "POST",
-    //     url: "saveImage.php",
-    //     data: {
-    //       uid: friendCache.me.id,
-    //       imgBase64: photo
-    //     }
-    //   }).done(function(o) {
-    //     console.log(o);
-    //     FBSharePhoto();
-    //     // If you want the file to be visible in the browser 
-    //     // - please modify the callback in javascript. All you
-    //     // need is to return the url to the file, you just saved 
-    //     // and than put the image in your browser.
-    //   });
-    // };
   });
 });
 
-function FBSharePhoto(url){
-  FB.ui(
-    {
-      method: 'feed',
-      name: 'Facebook Dialogs',
-      link: 'http://54.254.165.1/dev/',
-      picture: url,//'http://54.254.165.1/dev/images/' + friendCache.me.id + '.png',
-      caption: 'Reference Documentation',
-      description: 'Dialogs provide a simple, consistent interface for applications to interface with users.'
-    },
-    function(response) {
-      if (response && response.post_id) {
-        alert('Post was published.');
-      } else {
-        alert('Post was not published.');
-      }
-    }
-  );
-}
-
-function FBInvite(){
-    FB.ui({method:'apprequests',
-      message:'Invite your FB friends to Join Personata!'
-    },
-    function(e){
-      // if(e){
-      //  dialog('<h2>Successfully Invited</h2><p>Thank you very very much.</p>',!0)
-      // }else{
-      //  dialog('<h2>Error</h2><p>Failed To Invite Persons</p>',!0)
-      // }
-    })
-}
-
 function switchView(view) {
+  $("#login-page").hide();
+  $('#main').show();
   $('.view').hide();
   $(view).show();
 }
