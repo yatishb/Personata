60df4459842ee0d6af1d4307a66c320967f23789
ZHANG YICHUAN <zyc452478749@yahoo.com.sg>
2014-09-05 12:13:19 +0800

    Remove files for beginning trials

--- lineGraphJSON.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$data = array('fields' => array('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'),
-					'thisMonth' => array(1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233),
-					'lastMonth' => array(21, 34, 55, 89, 144, 233, 1, 2, 3, 5, 8, 13));
-
-	print json_encode($data);
-
-?>
--- pieGraphJSON.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$data = array('fields' => array('Photo', 'Video', 'SWF', 'Link', 'Status'),
-				'data' => array(0.1, 0.21, 0.29, 0.18, 0.22));
-
-	print json_encode($data);
-
-?>
